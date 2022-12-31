64e8eb96e78aa3fc45ddb86087db20684298d4f4
Dan Biscup <dabiscup@microsoft.com>
2022-12-31 15:46:28 -0600

    test

---
+++ mvc/mvc2powerapp/mvc2powerapp/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace mvc2powerapp.Models
+{
+    public class HomeViewModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
